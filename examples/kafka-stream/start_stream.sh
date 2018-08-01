86c3aad49a4c0911bd60a361be5e711d925793ae
William Streaker <william.streaker@envieta.com>
2018-08-01 15:33:01 -0400

    Fix kafka-stream example script

--- examples/kafka-stream/start_stream.sh
+++ examples/kafka-stream/start_stream.sh
@@ -1,4 +1,4 @@
 cd kafka-producer
-java -jar kafka-producer.jar kafka-producer.properties &>> stream.log &
+java -jar kafka-producer-1.0-jar-with-dependencies.jar kafka-producer.properties &>> stream.log &
 
 
